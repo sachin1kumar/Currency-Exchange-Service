a1bdc25d4a63371fe5abad5273732345e8b8e807
Sachin <sachinkmr375@gmail.com>
2021-02-16 21:38:52 +0530

    Getting currency exchange rate.

--- src/main/java/com/currency/service/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/currency/service/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,2 +1,21 @@
-package com.currency.service.currencyexchangeservice.controller;public class CurrencyExchangeController {
+package com.currency.service.currencyexchangeservice.controller;
+
+import com.currency.service.currencyexchangeservice.model.CurrencyExchange;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @GetMapping("currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange getCurrencyExchange(
+            @PathVariable String from,
+            @PathVariable String to
+    ) {
+        return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+    }
+
 }
--- src/main/java/com/currency/service/currencyexchangeservice/model/CurrencyExchange.java
+++ src/main/java/com/currency/service/currencyexchangeservice/model/CurrencyExchange.java
@@ -1,2 +1,50 @@
-package com.currency.service.currencyexchangeservice.model;public class CurrencyExchange {
+package com.currency.service.currencyexchangeservice.model;
+
+import java.math.BigDecimal;
+
+public class CurrencyExchange {
+
+    private long id;
+    private String from;
+    private String to;
+    private BigDecimal ConversionMultiple;
+
+    public CurrencyExchange(long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.ConversionMultiple = conversionMultiple;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return ConversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        ConversionMultiple = conversionMultiple;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }

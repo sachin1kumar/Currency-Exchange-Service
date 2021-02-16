d127ffc236bf93c29a85c4ed4bacc2a3171e664d
Sachin <sachinkmr375@gmail.com>
2021-02-16 22:41:27 +0530

    Added support for in-mem database.

--- src/main/java/com/currency/service/currencyexchangeservice/model/CurrencyExchange.java
+++ src/main/java/com/currency/service/currencyexchangeservice/model/CurrencyExchange.java
@@ -1,11 +1,18 @@
 package com.currency.service.currencyexchangeservice.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
 
+    @Id
     private long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal ConversionMultiple;
     private String environment;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into currency_exchange
+(id, conversion_multiple, environment, currency_from, currency_to)
+values(1000, 65, '', 'USA', 'INR');
+
+insert into currency_exchange
+(id, conversion_multiple, environment, currency_from, currency_to)
+values(1001, 63, '', 'USA', 'INR');
+
+insert into currency_exchange
+(id, conversion_multiple, environment, currency_from, currency_to)
+values(1002, 67, '', 'USA', 'INR');
